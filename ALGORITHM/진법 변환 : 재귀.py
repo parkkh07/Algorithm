0269c65b664f75ac0330ad5119dd6c7650d00236
parkkh07 <usparkkh@gmail.com>
2022-02-26 17:04:44 +0900

    진법 변환

---
+++ ALGORITHM/진법 변환 : 재귀.py
@@ -0,0 +1,19 @@
+n = int(input())
+a = int(input())
+t = 1
+while 1:
+    t *= a
+    if t * a > n:
+        break
+
+def binary(t,n):
+    if t == 0:
+        return    
+    print(n // t ,end=' ')
+    n -= (n // t) * t
+    t //= a
+    binary(t,n)
+    
+binary(t,n)
+
+
