83642707e217d4d5860b9d0729906b1395bd20fa
parkkh07 <usparkkh@gmail.com>
2022-03-02 00:36:21 +0900

    https://www.acmicpc.net/problem/15651

---
+++ Back tracking/BK 15651.py
@@ -0,0 +1,13 @@
+def func(k):
+    if k == m:
+        for i in range(m):
+            print(arr[i],end=' ')
+        print('\n',end='')
+        return
+    for i in range(1,n+1):
+        arr[k] = i
+        func(k+1)
+
+n,m = map(int,input().split(' '))
+arr = [0 for _ in range(10)]
+func(0)
