1bc4d77bee03b9b009b83038d8ef252621ac2235
parkkh07 <usparkkh@gmail.com>
2022-03-02 00:44:09 +0900

    https://www.acmicpc.net/problem/15654

---
+++ Back tracking/BK 15654.py
@@ -0,0 +1,18 @@
+def func(k):
+    if k == m:
+        for i in range(m):
+            print(arr[i],end=' ')
+        print('\n',end='')
+        return
+    for i in n_list:
+        if not isused[i]:
+            arr[k] = i
+            isused[i] = 1
+            func(k+1)
+            isused[i] = 0
+
+n,m = map(int,input().split(' '))
+n_list = sorted(list(map(int,input().split(' '))))
+arr = [0 for _ in range(10)]
+isused = [0 for _ in range(10001)]
+func(0)
